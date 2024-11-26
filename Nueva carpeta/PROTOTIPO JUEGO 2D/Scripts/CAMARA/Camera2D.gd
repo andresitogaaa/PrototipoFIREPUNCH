c00b9805d09f3738f29c2e06b9335ceaae433f3a
andresitogaaa <andresollantaysincherodas@gmail.com>
2024-11-26 16:42:26 -0500

    CamaraError

--- Nueva carpeta/PROTOTIPO JUEGO 2D/Scripts/CAMARA/Camera2D.gd
+++ Nueva carpeta/PROTOTIPO JUEGO 2D/Scripts/CAMARA/Camera2D.gd
@@ -9,9 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	camera_panning()
 
 func camera_panning():
 	position = player.position
 	var x = floor(position.x / 320) * 320
 	var y = floor(position.y / 180) * 180
+
+	position = Vector2(x,y)
+
+	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "position", Vector2(x,y), 0.14)
