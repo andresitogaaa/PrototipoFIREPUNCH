75f54882125a80a243b48d54e96f36999ed46afc
andresitogaaa <andresollantaysincherodas@gmail.com>
2024-11-27 13:42:33 -0500

    CamaraFix

--- Nueva carpeta/PROTOTIPO JUEGO 2D/Scripts/CAMARA/Camera2D.gd
+++ Nueva carpeta/PROTOTIPO JUEGO 2D/Scripts/CAMARA/Camera2D.gd
@@ -1,6 +1,8 @@
 extends Camera2D
 
 @onready var player = $"../Player"
+@export var camera: camera_state
+enum camera_state {FOLLOW, PANNING}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,9 +11,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	camera_panning()
+	match camera:
+		camera_state.FOLLOW:
+			camera_follow()
+		camera_state.PANNING:
+			camera_panning()
 
 func camera_panning():
+	anchor_mode = Camera2D.ANCHOR_MODE_FIXED_TOP_LEFT
 	position = player.position
 	var x = floor(position.x / 320) * 320
 	var y = floor(position.y / 180) * 180
@@ -20,3 +27,7 @@ func camera_panning():
 
 	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "position", Vector2(x,y), 0.14)
+
+func camera_follow():
+	anchor_mode = Camera2D.ANCHOR_MODE_DRAG_CENTER
+	position = player.position
